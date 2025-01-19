c347e5a0034b2d14dd10bde411c66d508c05afd9
Ansh0821 <ansh2020kumar@gmail.com>
2025-01-19 14:02:03 +0530

    Updated frontend url

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,9 @@ from app import create_app  # Import the create_app function
 from flask_cors import CORS
 
 app = create_app()  # Create an instance of your Flask app
-CORS(app, origins=["http://localhost:5173", "https://scrapetheweb.netlify.app/scraper"], supports_credentials=True)
+
+# Update the CORS configuration to match both frontend and backend
+CORS(app, origins=["https://scrapetheweb.netlify.app"])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Default to port 5000 for local testing
