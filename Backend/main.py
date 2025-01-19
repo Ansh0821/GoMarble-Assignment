bc96c92708dba6910a96b783303c33cae111a20e
Ansh0821 <ansh2020kumar@gmail.com>
2025-01-19 13:30:24 +0530

    Added frontend url in CORS

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,7 @@ from app import create_app  # Import the create_app function
 from flask_cors import CORS
 
 app = create_app()  # Create an instance of your Flask app
-CORS(app, origins=["http://localhost:5173", "https://your-frontend-deployment-url"])
+CORS(app, origins=["http://localhost:5173", "https://scrapetheweb.netlify.app/"])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Default to port 5000 for local testing
